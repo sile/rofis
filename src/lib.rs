9aae2d3662566e07f1a3d4a0d8758e90f8f1c98a
Takeru Ohta <phjgt308@gmail.com>
2018-06-24 15:16:11 +0900

    Add `error` module

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,11 @@ extern crate trackable;
 #[macro_use]
 extern crate slog;
 
+pub use error::{Error, ErrorKind};
 pub use file_server::FileServer;
 
+mod error;
 mod file_server;
+
+/// This crate specific `Result` type.
+pub type Result<T> = std::result::Result<T, Error>;
