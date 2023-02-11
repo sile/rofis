848a0d5070e50616806300dea55be59f37cff5c6
Takeru Ohta <phjgt308@gmail.com>
2023-02-11 11:46:37 +0900

    Initialize files

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,1 @@
-//! A read-only, puny HTTP file server.
-#![warn(missing_docs)]
-#[macro_use]
-extern crate trackable;
-#[macro_use]
-extern crate slog;
 
-pub use error::{Error, ErrorKind};
-pub use file_server::FileServer;
-
-mod error;
-mod file_server;
-
-/// This crate specific `Result` type.
-pub type Result<T> = std::result::Result<T, Error>;
--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,12 @@
-#[macro_use]
-extern crate clap;
-#[macro_use]
-extern crate trackable;
+use clap::Parser;
+use orfail::OrFail;
 
-use clap::Arg;
-use fibers::{Executor, Spawn, ThreadPoolExecutor};
-use fibers_http_server::ServerBuilder;
-use futures::Future;
-use rofis::FileServer;
-use sloggers::terminal::TerminalLoggerBuilder;
-use sloggers::Build;
+#[derive(Debug, Parser)]
+#[clap(version)]
+struct Args {}
 
-macro_rules! try_parse {
-    ($expr:expr) => {
-        track_try_unwrap!(track_any_err!($expr.parse()))
-    };
-}
-
-fn main() {
-    let matches = app_from_crate!()
-        .arg(
-            Arg::with_name("PORT")
-                .long("port")
-                .short("p")
-                .takes_value(true)
-                .default_value("8080"),
-        )
-        .arg(
-            Arg::with_name("LOG_LEVEL")
-                .long("log-level")
-                .takes_value(true)
-                .default_value("info")
-                .possible_values(&["debug", "info", "warning", "error"]),
-        )
-        .arg(Arg::with_name("DAEMONIZE").long("daemonize").short("d"))
-        .get_matches();
-    let port = matches.value_of("PORT").expect("Never fails");
-    let bind_addr = try_parse!(format!("0.0.0.0:{}", port));
-    let log_level = try_parse!(matches.value_of("LOG_LEVEL").expect("Never fails"));
-    let logger = track_try_unwrap!(TerminalLoggerBuilder::new().level(log_level).build());
-    let daemonize = matches.is_present("DAEMONIZE");
-    if daemonize {
-        let current_dir = track_try_unwrap!(track_any_err!(std::env::current_dir()));
-        let d = daemonize::Daemonize::new().working_directory(current_dir);
-        track_try_unwrap!(track_any_err!(d.start()));
-    }
-
-    let executor = track_try_unwrap!(track_any_err!(ThreadPoolExecutor::with_thread_count(1)));
-    let mut builder = ServerBuilder::new(bind_addr);
-    builder.logger(logger.clone());
-
-    let file_server = FileServer::new(logger);
-    track_try_unwrap!(file_server.register(&mut builder));
-
-    let http_server = builder.finish(executor.handle());
-    executor.spawn(http_server.map_err(|e| panic!("{}", e)));
-
-    track_try_unwrap!(track_any_err!(executor.run()));
+fn main() -> orfail::Result<()> {
+    env_logger::try_init().or_fail()?;
+    let _args = Args::parse();
+    Ok(())
 }
